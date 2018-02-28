477d8b56a7d00250a5ff48100d89462172c34319
= <kj.tokarz@gmail.com>
2018-02-27 20:59:30 -0500

    Move files and add pg library

--- routes/lifting.js
+++ routes/lifting.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
 
 router.get('/', function (req, res, next) {
     res.render('lifting', {title: "Lifting"});
+});
+
+router.post("/exercise", function (req, res, next) {
+    sanitize('exercise-name', 'weight', 'sets', 'reps').trim().escape();
+
+    
 });
 
 module.exports = router;
