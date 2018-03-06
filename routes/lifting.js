3f8258b6592effd659f60931e88b48126a1a8e26
= <kj.tokarz@gmail.com>
2018-03-05 21:16:57 -0500

    Write code to begin creating lifting exercise records

--- routes/lifting.js
+++ routes/lifting.js
@@ -2,15 +2,18 @@ var express = require("express");
 var router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 const { matchedData, sanitize } = require('express-validator/filter');
+const LiftingExercise = require('../models/lifting-exercise');
+const uuidv5 = require('uuid/v5');
 
 router.get('/', function (req, res, next) {
     res.render('lifting', {title: "Lifting"});
 });
 
 router.post("/exercise", function (req, res, next) {
-    sanitize('exercise-name', 'weight', 'sets', 'reps').trim().escape();
-
-    
+    sanitize('exerciseName', 'weight', 'sets', 'reps').trim().escape();
+    var id = uuidv5('anonymousWorkoutTracker.com', uuidv5.URL);
+    var liftingExercise = new LiftingExercise(id, req.session.id, req.body.exerciseName, req.body.weight, req.body.sets, req.body.reps);
+    liftingExercise.create();
 });
 
 module.exports = router;
